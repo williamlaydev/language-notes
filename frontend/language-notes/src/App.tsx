ed0090c5f4f2f4d33ef5a70808cee531056c7494
William Lay <willlaydev@gmail.com>
2024-12-18 18:29:58 +1100

    TranslationCard rendering

--- frontend/language-notes/src/App.tsx
+++ frontend/language-notes/src/App.tsx
@@ -1,17 +1,55 @@
 import { useState } from 'react'
 import './App.css'
 import TranslationCard from './components/TranslationCard'
+import { translationCards } from './database'
 
 function App() {
+  let cards = [
+    {
+      english: "",
+      meaning: "",
+      translated: "",
+      language: "chinese"
+    }, 
+    ...translationCards
+  ]
+
   return (
     <>
-      <div>
-        <TranslationCard language="chinese"/>
-        <TranslationCard language="chinese"/>
-        <TranslationCard language="chinese"/>
+      <div className="flex flex-row w-full h-screen px-4 py-4">
+        {/* Left Section: 40% */}
+        <div className="w-2/5 bg-gray-100 p-4">
+          <h2 className="text-lg font-bold">Nav area</h2>
+        </div>
+  
+        {/* Right Section: 60% */}
+        <div className="w-3/5 p-4">
+          <div className="grid grid-cols-6 grid-rows-5 gap-4">
+            {cards.map((card, index) => {
+              return card.english === "" ? (
+                <TranslationCard
+                  key={index}
+                  language={card.language}
+                  english=""
+                  meaning=""
+                  translated=""
+                />
+              ) : (
+                <TranslationCard
+                  key={index}
+                  language={card.language}
+                  english={card.english}
+                  meaning={card.meaning}
+                  translated={card.translated}
+                />
+              );
+            })}
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
+  
 }
 
 export default App
--- frontend/language-notes/src/components/TranslationCard.tsx
+++ frontend/language-notes/src/components/TranslationCard.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
 
 type TranslationCardProps = {
+    english: string
+    meaning: string
+    translated: string
     language: string
 }
 
@@ -13,23 +16,27 @@ type TranslatedInfo = {
 
 function TranslationCard(props: TranslationCardProps) {
     const [translatedInfo, setTranslatedInfo] = useState<TranslatedInfo>({
-        english: "",
-        meaning: "",
-        translated: "",
+        english: props.english,
+        meaning: props.meaning,
+        translated: props.translated,
         language: props.language
     })
 
-    const [isTranslated, setIsTranslated] = useState(false)
+    const [isTranslated, setIsTranslated] = useState(() => {
+        return props.english ? true : false
+    })
 
     const handleUnfocus = async () => {
-        if (!isTranslated) {
-            const res = await translateEnglishWord(translatedInfo.english, translatedInfo.language)
-            setIsTranslated(true)
-            setTranslatedInfo(prev => ({
-                ...prev,
-                meaning: res.meaning,
-                translated: res.translated
-            }))
+        if (props.english) {
+            if (!isTranslated) {
+                const res = await translateEnglishWord(translatedInfo.english, translatedInfo.language)
+                setIsTranslated(true)
+                setTranslatedInfo(prev => ({
+                    ...prev,
+                    meaning: res.meaning,
+                    translated: res.translated
+                }))
+            }
         }
     }
 
@@ -46,18 +53,24 @@ function TranslationCard(props: TranslationCardProps) {
             }
         }
     }
+    
     return (
-        <div className="max-w-sm p-4 bg-yellow-100 border border-yellow-300 rounded-lg shadow-md">
-            <input 
-                className="w-full p-2 mb-4 bg-transparent border-b-2 border-gray-300 focus:outline-none focus:border-yellow-500"
-                type="text"
-                onChange={(e) => setTranslatedInfo(prev => ({
-                    ...prev,
-                   english: e.target.value 
-                }))}
-                onBlur={() => handleUnfocus()}
-                onKeyDown={(e) => handleEnter(e)}
-            />
+        <div className="max-w-sm p-4 bg-yellow-100 border border-yellow-300 shadow-md text-center">
+            {
+                // Handle if translated
+                isTranslated ? <h3 className="text-lg font-medium text-gray-800">{translatedInfo.english}</h3>
+                : <input 
+                    className="w-full p-2 mb-4 bg-transparent border-b-2 border-gray-300 focus:outline-none focus:border-yellow-500"
+                    type="text"
+                    onChange={(e) => setTranslatedInfo(prev => ({
+                        ...prev,
+                        english: e.target.value 
+                    }))}
+                    onBlur={() => handleUnfocus()}
+                    onKeyDown={(e) => handleEnter(e)}
+                    placeholder={translatedInfo.english}
+                />
+            }
             <h3 className="text-lg font-medium text-gray-800" >{translatedInfo.translated}</h3>
             <h3 className="text-sm text-gray-600">{translatedInfo.meaning}</h3>
         </div>
